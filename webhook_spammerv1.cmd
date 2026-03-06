48dc9ee35df89807f6a3e8f64f6c3d5db482570c
slynx <qwixzaasasasas@gmail.com>
2026-03-06 15:54:08 +0100

    Add files via upload

---
+++ webhook_spammerv1.cmd
@@ -0,0 +1,16 @@
+@echo off
+title webhook spammer by slynx
+color 3
+cls
+echo btw if you press enter after filling the webhok url
+echo and message shit it will spam all the time until you disable this cmd
+echo and one more thing you CAN be rate limited or
+echo banned of discord api
+echo.
+set /p "wh=webhook url: "
+set /p "msg=message: "
+
+:spam
+curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"%msg%\"}" %wh%
+echo sent message
+goto spam
